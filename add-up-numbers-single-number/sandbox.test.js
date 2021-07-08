489c21cc7d797a92e89ea1088202ac3eccbb2758
Michael Tesfay <75224973+Mika215@users.noreply.github.com>
2021-07-08 10:04:26 +0200

    pushing add-Up-Numbers  folder

---
+++ add-up-numbers-single-number/sandbox.test.js
@@ -0,0 +1,47 @@
+
+//[_sir](https://edabit.com/user/F7iZc3vpy7d9ALD6D)
+
+function addUp(num) {
+    return (num * (num + 1))/2;
+  }
+
+
+
+//[Gabriel](https://edabit.com/user/akYGmQ9iZcZm2d8BP)
+
+function addUp(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+//[Carbon](https://edabit.com/user/2PaATWytPkFGENDqK)
+//this is exactly the same us Gabriel's however i chose this one
+// because the variable names are more clear comparing to Gabriel's(num, and sum are quite similar and tricky) 
+function addUp(num) {
+    var total = 0;
+    for (var i = 1; i <= num; i++) {
+      total += i;
+    }
+    return total;
+  }
+  
+
+
+
+
+//[Korean](https://edabit.com/user/REGpkw8KfARygwCmB)
+
+function addUp(num) {
+    var a =0;
+    for(var i =num; i > 0; i--){
+      a+= i;
+    }
+    return a;
+  }
+
+
+
+[]()
